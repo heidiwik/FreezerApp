46b81e3fddbd5a467f86b02533fe864b9a1060a2
Heidi Wikman <heidi.wikman@kapsi.fi>
2025-05-18 17:26:25 +0300

    Added grouping for items with same name

--- FreezerApp/FreezerItem.cs
+++ FreezerApp/FreezerItem.cs
@@ -1,9 +1,12 @@
-﻿namespace FreezerApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreezerApp.Models
 {
     public class FreezerItem
     {
         public Guid Id { get; set; }
         public int? BoxId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
         public required string Name { get; set; }
         public int Quantity { get; set; } = 1;
         public string? Location { get; set; }
